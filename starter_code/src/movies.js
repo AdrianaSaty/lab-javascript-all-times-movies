1cdbbc3d25cc4ef55ed4bcab08e9a8b5e5589916
Adriana Saty Bertelli <Adria@Adria-2.local>
2019-06-27 00:55:18 -0300

    ex1 done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,46 @@
 /* eslint no-restricted-globals: 'off' */
+
+// const movies = require('./data');
+
 // Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes(movies){
+    let HoursToMinutesMovies = JSON.parse(JSON.stringify(movies));
+
+    var HoursToMinutesMoviesCopy = HoursToMinutesMovies.map(function(num) {
+        if((num.duration).length === 8 || (num.duration).length === 7 ){
+            let hourDuration = parseInt(num.duration.substr(0,1)) * 60;
+            hourDuration += parseInt((num.duration.substr(2)));
+            num.duration = hourDuration;
+            return num; 
+        } else if ( (num.duration).indexOf("h") !== -1){
+            num.duration = parseInt(num.duration) * 60;
+            return num;
+        } else {
+            num.duration = parseInt(num.duration);
+            return num;
+        }
+
+    });
+
+    return HoursToMinutesMoviesCopy;
+}
 
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage(movies){
+    let avarage = JSON.parse(JSON.stringify(movies));
+    let index = 0;
+    var sum = movies.reduce(function(accumulator, num){
+      num.rate = parseFloat(num.rate);
+      index ++;
+      return (accumulator + num.rate);
+    },0 );
 
+    console.log((sum/index).toFixed(2));
+    return (sum/index).toFixed(2);
+}
+
+ratesAverage(movies)
 
 // Get the average of Drama Movies
 
